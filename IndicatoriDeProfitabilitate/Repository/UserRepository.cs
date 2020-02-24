632a4abdc6eca842fd30fe387f7f2a152f3610d5
Carmen Cenan <carmencenan@gmail.com>
2020-02-24 21:54:22 +0200

    Commit

---
+++ IndicatoriDeProfitabilitate/Repository/UserRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IndicatoriDeProfitabilitate.Repository
+{
+    public class UserRepository
+    {
+    }
+}
